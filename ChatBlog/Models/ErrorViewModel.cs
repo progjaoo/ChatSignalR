2f2f2ef023c15bf66b06f165a4ee9e015d2f794d
Joao marcos valente <joaomarcoslars.81@gmail.com>
2022-12-03 12:33:53 -0300

    chat desenvolvido em SignalR com simples design

---
+++ ChatBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChatBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
